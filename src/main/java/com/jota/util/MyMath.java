ed5b880e696300ca2e7977b1eaf71493a2c86c01
JorgeTamara22 <jorgetape@hotmail.com>
2021-05-12 05:18:20 -0500

    crea test1

--- src/main/java/com/jota/util/MyMath.java
+++ src/main/java/com/jota/util/MyMath.java
@@ -5,9 +5,12 @@ import java.util.List;
 public class MyMath {
 
 	public static Number calculate(String operator,List<Number> numbers) throws Exception{
-		Number total=0;
+		Number total=null;
 		try {
 			for(Number number:numbers) {
+				if(total==null) {
+					total=number.doubleValue();
+				}else {
 				if("+".equals(operator)) {
 					total=total.doubleValue()+number.doubleValue();
 				}else if("-".equals(operator)) {
@@ -18,6 +21,7 @@ public class MyMath {
 					total=total.doubleValue()/number.doubleValue();
 				}else if("^".equals(operator)) {
 					total=Math.pow(total.doubleValue(),number.doubleValue());
+				}
 				}
 			}
 		}catch(Exception e) {
